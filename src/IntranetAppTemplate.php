a0db90ba3a574d4014c98cb1fe2a09d4bf7826c0
aleex1848 <halycon01@gmail.com>
2025-08-01 07:18:00 +0200

    added IntranetAppInterface

--- src/IntranetAppTemplate.php
+++ src/IntranetAppTemplate.php
@@ -1,5 +1,33 @@
 <?php
 
 namespace Hwkdo\IntranetAppTemplate;
+use Hwkdo\IntranetAppBase\Interfaces\IntranetAppInterface;
+use Illuminate\Support\Collection;
 
-class IntranetAppTemplate {}
+class IntranetAppTemplate implements IntranetAppInterface 
+{
+    public static function app_name(): string
+    {
+        return 'Template';
+    }
+
+    public static function app_icon(): string
+    {
+        return 'magnifying-glass';
+    }
+
+    public static function identifier(): string
+    {
+        return 'template';
+    }
+
+    public static function roles_admin(): Collection
+    {
+        return collect(config('intranet-app-template.roles.admin'));
+    }
+
+    public static function roles_user(): Collection
+    {
+        return collect(config('intranet-app-template.roles.user'));
+    }    
+}
